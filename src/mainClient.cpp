573ed0f0500f3480f3c8cd7ed418c3bb1279129c
veg4 <florian@wuerrer.com>
2013-10-14 01:25:46 +0200

    Update mainClient.cpp

--- src/mainClient.cpp
+++ src/mainClient.cpp
@@ -14,6 +14,9 @@
 #include <signal.h>
 #include <sys/wait.h>
 #include <string>
+#include <stdio.h>
+#include <string.h>
+
 
 /** macros **/
 /** MAXLINE LENGTH**/
@@ -41,16 +44,87 @@ int main(int argc, char **argv){
 	
 	Client cli = Client();
 
-	std::cout << "Connectiing to Server" << std::endl;
+	std::cout << "Connecting to Server" << std::endl;
 	try{
 		cli.Connect(hostname, port);
-
+		
+		char buffer [1024];
+		char bufferread [1024];
 		char message[1024];
-  		std::cout << "Please, enter your name(max. 1024 chars): ";
-  		std::cin.getline (message,1024);
-
+		//bzero((char *) &serv_addr, sizeof(serv_addr));
+		int userOption;
+		bool check;
+		check = true;
+		while (check)
+		{
+			std::cout << "Willkommen! Welche Operation wuerden Sie gerne ausfuehren? 1.SEND, 2.LIST, 3.READ, 4.DEL  0.Exit \n";
+			std::cin >> userOption;
+			switch(userOption)
+			  {
+				 case 1 :
+					std::cout << "Bitte geben Sie den Sender ein:(max. 8 chars)"; 
+					strcpy (message,"SEND \n");
+					std::cin.ignore();
+					std::cin.getline(buffer,8); 
+					strcat (message,buffer);
+					strcat (message,"\n");
+					std::cout << "Bitte geben Sie den Empfaenger ein:(max. 8 chars)"; 
+					std::cin.getline(buffer,8);
+					strcat (message,buffer);
+					strcat (message,"\n");
+					std::cout << "Bitte geben Sie den Betreff ein:(max. 80 chars)"; 
+					std::cin.getline(buffer,80);
+					strcat (message,buffer);
+					strcat (message,"\n");
+					std::cout << "Bitte geben Sie die Nachricht ein:(max. 920 chars)"; 
+					std::cin.getline(buffer,900);
+					strcat (message,buffer);
+					strcat (message,"\n");
+					strcat (message,".\n");
+					break;
+				 case 2 :
+					std::cout << "Bitte geben Sie den gewünschten Username ein:(max. 8 chars)"; 
+					strcpy (message,"LIST \n");
+					std::cin.ignore();
+					std::cin.getline(buffer,8); 
+					strcat (message,buffer);
+					strcat (message,"\n");
+					break;
+				 case 3 :
+					std::cout << "Bitte geben Sie den gewünschten Username ein:(max. 8 chars)"; 
+					strcpy (message,"READ \n");
+					std::cin.ignore();
+					std::cin.getline(buffer,8); 
+					strcat (message,buffer);
+					strcat (message,"\n");
+					std::cout << "Bitte geben Sie die Nummer der Nachricht an:(max. 8 chars)"; 
+					std::cin.getline(buffer,8);
+					strcat (message,buffer);
+					strcat (message,"\n");
+					break;
+				 case 4 :
+					std::cout << "Bitte geben Sie den gewünschten Username ein:(max. 8 chars)"; 
+					strcpy (message,"DEL \n");
+					std::cin.ignore();
+					std::cin.getline(buffer,8); 
+					strcat (message,buffer);
+					strcat (message,"\n");
+					std::cout << "Bitte geben Sie die Nummer der Nachricht an:(max. 8 chars)"; 
+					std::cin.getline(buffer,8);
+					strcat (message,buffer);
+					strcat (message,"\n");
+					break;
+				 case 0 :
+					check = false;
+					break;
+				 default:
+					std::cout << "Sie müssen eine Zahl zwischen 1 und 4 für die jeweilige Operation eingeben.";
+					break;
+			  }
+		
 		cli.SendMessage(message,sizeof(message));
-
+		//cli.ReadMessage();
+	}
 	} catch(const char *ex){
 		std::cerr << ex << std::endl;
 		return EXIT_FAILURE;
