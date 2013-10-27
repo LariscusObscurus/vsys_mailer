d23980a6c0ac01af6d199704393035133f580cc5
Leonhardt Schwarz <if12b076@technikum--wien.at>
2013-10-27 21:24:05 +0100

    Client angepasst

--- src/client.cpp
+++ src/client.cpp
@@ -10,89 +10,75 @@
 #include <fstream>
 
 
-int sockfd = -1;
 
-Client::Client() {
+Client::Client() :
+	m_sockfd(-1)
+{
+
 }
 
-Client::~Client(){
-	close(sockfd);
+Client::~Client()
+{
+	close(m_sockfd);
 }
 
-int Client::Connect (char* const server_hostname, const char* server_service){
-        struct addrinfo* ai,* ai_sel = NULL;
-        struct addrinfo hints;
-        int err;
+int Client::Connect (char* const server_hostname, const char* server_service)
+{
+	struct addrinfo* ai,* ai_sel = NULL;
+	struct addrinfo hints;
+	int err;
 
-        hints.ai_flags = AI_PASSIVE;
-        hints.ai_family = AF_UNSPEC;
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_protocol = IPPROTO_TCP;
-        hints.ai_addrlen = 0;
-        hints.ai_addr = NULL;
-        hints.ai_canonname = NULL;
-        hints.ai_next = NULL;
+	hints.ai_flags = AI_PASSIVE;
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_addrlen = 0;
+	hints.ai_addr = NULL;
+	hints.ai_canonname = NULL;
+	hints.ai_next = NULL;
 
-        //resolve ipaddr
-        if((err = getaddrinfo(server_hostname, server_service, &hints, &ai)) != 0){
-                (void) fprintf(stderr, "ERROR:  %s\n", gai_strerror(err));
-                exit(EXIT_FAILURE);
-        }
+	//resolve ipaddr
+	if((err = getaddrinfo(server_hostname, server_service, &hints, &ai)) != 0){
+		(void) fprintf(stderr, "ERROR:  %s\n", gai_strerror(err));
+		return -1;
+	}
 
-        if(ai == NULL){
-                (void) fprintf(stderr, "Could not resolve host %s.\n", server_hostname);
-                freeaddrinfo(ai);
-                exit(EXIT_FAILURE);
-        }
+	if(ai == NULL){
+		(void) fprintf(stderr, "Could not resolve host %s.\n", server_hostname);
+		freeaddrinfo(ai);
+		return -1;
+	}
 
-        ai_sel = ai;
+	ai_sel = ai;
 
-        //creat socket
-        if((sockfd = socket(ai_sel->ai_family, ai_sel->ai_socktype, ai->ai_protocol)) < 0){
-                (void) fprintf(stderr, "Socket creation failed\n");
-                close(sockfd);
-                freeaddrinfo(ai);
-                exit(EXIT_FAILURE);
-        }
+	//creat socket
+	if((m_sockfd = socket(ai_sel->ai_family, ai_sel->ai_socktype, ai->ai_protocol)) < 0){
+		(void) fprintf(stderr, "Socket creation failed\n");
+		freeaddrinfo(ai);
+		return -1;
+	}
 
-        //connect to server
-        if(connect(sockfd, ai_sel->ai_addr, ai_sel->ai_addrlen) < 0){
-                (void) close(sockfd);
-                freeaddrinfo(ai);
-                (void) fprintf(stderr, "Connection failed.\n");
-                close(sockfd);
-                exit(EXIT_FAILURE);
-        }
-        
+	//connect to server
+	if(connect(m_sockfd, ai_sel->ai_addr, ai_sel->ai_addrlen) < 0){
+		(void) close(m_sockfd);
+		freeaddrinfo(ai);
+		(void) fprintf(stderr, "Connection failed.\n");
+		return -1;
+	}
+
 	freeaddrinfo(ai);
-        //freeaddrinfo(ai_sel);
+	//freeaddrinfo(ai_sel);
 
-	return sockfd;
+	return m_sockfd;
 }
 
-int Client::SendMessage(char* const message,int size){
-	int bytes_sent = write(sockfd, message, size);
-                if(bytes_sent < 0){
-                        printf("Write Error.");
-                        (void) close(sockfd);
-                        exit(EXIT_FAILURE);
-                }
-        
+int Client::SendMessage(const char *message,int size)
+{
+	int bytes_sent = send(m_sockfd, message, size, 0);
+		if(bytes_sent < 0){
+			printf("Send Error.");
+			return -1;
+		}
+
 	return 0;
 }
-
-/*void Client::ReadMessage(){
-	close(sockfd);
-	long bytesReceived;
-	char *buf = new char[BUFFERSIZE];
-
-	if((bytesReceived = recv(sockedfd,buf ,BUFFERSIZE, 0)) == -1) {
-		delete[] buf;
-		sendERR(childfd);
-		return;
-	}
-	buf[bytesReceived] = '\0';
-
-	std::cout << buf << std::endl;
-	m_buffer = std::string(buf);
-}*/
--- src/mainClient.cpp
+++ src/mainClient.cpp
@@ -1,11 +1,11 @@
 /**
- * @file mainClient.cpp
- * @author Florian Wuerrer <if13b077@technikum-wien.at>
- * @date 11.10.2013
- *
- * @brief client-main for client-server application
- * 
- **/
+* @file mainClient.cpp
+* @author Florian Wuerrer <if13b077@technikum-wien.at>
+* @date 11.10.2013
+*
+* @brief client-main for client-server application
+*
+**/
 
 /** include **/
 #include <cstdlib>
@@ -18,21 +18,7 @@
 #include <string.h>
 
 
-/** macros **/
-/** MAXLINE LENGTH**/
-#define LINEMAX 1024
-/** Error message for close**/
-#define CLOSEERRORMSG "close error\n"
-/** Error message for fclose**/
-#define FCLOSEERRORMSG "fclose error\n"
-
-
-void signalHandler(int s){
-	while(waitpid(-1,NULL, WNOHANG) > 0);
-}
-
-int main(int argc, char **argv){	 
-	struct sigaction sa;
+int main(int argc, char **argv){
 
 	if(argc < 3) {
 		std::cout << "Server and/or Portnumber given!\nusage:" << argv[0] << " hostname port" << std::endl;
@@ -41,94 +27,79 @@ int main(int argc, char **argv){
 
 	char *hostname = argv[1];
 	char *port = argv[2];
-	
+
 	Client cli = Client();
 
 	std::cout << "Connecting to Server" << std::endl;
-	try{
+
+
+	std::string buffer;
+	std::string message;
+	//bzero((char *) &serv_addr, sizeof(serv_addr));
+	int userOption;
+	bool check;
+	check = true;
+	while (check)
+	{
+		std::cout << "Willkommen! Welche Operation wuerden Sie gerne ausfuehren? 1.SEND, 2.LIST, 3.READ, 4.DEL  0.Exit \n";
+		std::cin >> userOption;
+		switch(userOption) {
+		case 1 :
+			std::cout << "Bitte geben Sie den Sender ein:(max. 8 chars)";
+			message += "SEND \n";
+			std::cin.ignore();
+			std::getline(std::cin,buffer);
+			message += buffer + "\n";
+			std::cout << "Bitte geben Sie den Empfaenger ein:(max. 8 chars)";
+			std::getline(std::cin,buffer);
+			message += buffer + "\n";
+			std::cout << "Bitte geben Sie den Betreff ein:(max. 80 chars)";
+			std::getline(std::cin,buffer);
+			message += buffer + "\n";
+			std::cout << "Bitte geben Sie die Nachricht ein:(max. 920 chars)";
+			std::getline(std::cin,buffer);
+			message += buffer + "\n" + ".\n";
+			break;
+		case 2 :
+			std::cout << "Bitte geben Sie den gewünschten Username ein:(max. 8 chars)";
+			message += "LIST \n";
+			std::cin.ignore();
+			std::getline(std::cin,buffer);
+			message += buffer + "\n" + ".\n";
+			break;
+		case 3 :
+			std::cout << "Bitte geben Sie den gewünschten Username ein:(max. 8 chars)";
+			message += "READ \n";
+			std::cin.ignore();
+			std::getline(std::cin,buffer);
+			message += buffer + "\n";
+			std::cout << "Bitte geben Sie die Nummer der Nachricht an:(max. 8 chars)";
+			std::getline(std::cin,buffer);
+			message += buffer + "\n" + ".\n";
+			break;
+		case 4 :
+			std::cout << "Bitte geben Sie den gewünschten Username ein:(max. 8 chars)";
+			message += "DEL \n";
+			std::cin.ignore();
+			std::getline(std::cin,buffer);
+			message += buffer + "\n";
+			std::cout << "Bitte geben Sie die Nummer der Nachricht an:(max. 8 chars)";
+			std::getline(std::cin,buffer);
+			message += buffer + "\n" + ".\n";
+			break;
+		case 0 :
+			check = false;
+			break;
+		default:
+			std::cout << "Sie müssen eine Zahl zwischen 1 und 4 für die jeweilige Operation eingeben.";
+			break;
+		}
 		cli.Connect(hostname, port);
-		
-		char buffer [1024];
-		char bufferread [1024];
-		char message[1024];
-		//bzero((char *) &serv_addr, sizeof(serv_addr));
-		int userOption;
-		bool check;
-		check = true;
-		while (check)
-		{
-			std::cout << "Willkommen! Welche Operation wuerden Sie gerne ausfuehren? 1.SEND, 2.LIST, 3.READ, 4.DEL  0.Exit \n";
-			std::cin >> userOption;
-			switch(userOption)
-			  {
-				 case 1 :
-					std::cout << "Bitte geben Sie den Sender ein:(max. 8 chars)"; 
-					strcpy (message,"SEND \n");
-					std::cin.ignore();
-					std::cin.getline(buffer,8); 
-					strcat (message,buffer);
-					strcat (message,"\n");
-					std::cout << "Bitte geben Sie den Empfaenger ein:(max. 8 chars)"; 
-					std::cin.getline(buffer,8);
-					strcat (message,buffer);
-					strcat (message,"\n");
-					std::cout << "Bitte geben Sie den Betreff ein:(max. 80 chars)"; 
-					std::cin.getline(buffer,80);
-					strcat (message,buffer);
-					strcat (message,"\n");
-					std::cout << "Bitte geben Sie die Nachricht ein:(max. 920 chars)"; 
-					std::cin.getline(buffer,900);
-					strcat (message,buffer);
-					strcat (message,"\n");
-					strcat (message,".\n");
-					break;
-				 case 2 :
-					std::cout << "Bitte geben Sie den gewünschten Username ein:(max. 8 chars)"; 
-					strcpy (message,"LIST \n");
-					std::cin.ignore();
-					std::cin.getline(buffer,8); 
-					strcat (message,buffer);
-					strcat (message,"\n");
-					break;
-				 case 3 :
-					std::cout << "Bitte geben Sie den gewünschten Username ein:(max. 8 chars)"; 
-					strcpy (message,"READ \n");
-					std::cin.ignore();
-					std::cin.getline(buffer,8); 
-					strcat (message,buffer);
-					strcat (message,"\n");
-					std::cout << "Bitte geben Sie die Nummer der Nachricht an:(max. 8 chars)"; 
-					std::cin.getline(buffer,8);
-					strcat (message,buffer);
-					strcat (message,"\n");
-					break;
-				 case 4 :
-					std::cout << "Bitte geben Sie den gewünschten Username ein:(max. 8 chars)"; 
-					strcpy (message,"DEL \n");
-					std::cin.ignore();
-					std::cin.getline(buffer,8); 
-					strcat (message,buffer);
-					strcat (message,"\n");
-					std::cout << "Bitte geben Sie die Nummer der Nachricht an:(max. 8 chars)"; 
-					std::cin.getline(buffer,8);
-					strcat (message,buffer);
-					strcat (message,"\n");
-					break;
-				 case 0 :
-					check = false;
-					break;
-				 default:
-					std::cout << "Sie müssen eine Zahl zwischen 1 und 4 für die jeweilige Operation eingeben.";
-					break;
-			  }
-		
-		cli.SendMessage(message,sizeof(message));
-		//cli.ReadMessage();
+		cli.SendMessage(message.c_str(),message.length());
+		message.clear();
 	}
-	} catch(const char *ex){
-		std::cerr << ex << std::endl;
-		return EXIT_FAILURE;
-	}
-	
-	return EXIT_SUCCESS;
+
+	//cli.ReadMessage();
+
+		return EXIT_SUCCESS;
 }
